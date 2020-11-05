c33312db369943cd6821949097db57fcbc90984f
Hyun O Sung <sierrahotel555@gmail.com>
2020-11-05 19:20:53 +0900

    201105 커밋 - 노드 위치 리스트로 작성 성공

---
+++ Assets/Scripts/C#/PlayerController_RNC.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_RNC : MonoBehaviour
+{
+    public float playerSpeed = 1.0f;
+    public float playerRot = 2.0f;
+    public GameObject Body;
+    public GameObject[] points;
+
+    int currentPoint;
+
+    static Vector2 currentPointPosition;
+
+
+    void CheckPoint()
+    {
+        if(currentPoint < points.Length)
+        {
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    Body.()
+        //}
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Body.transform.forward * playerSpeed * Time.deltaTime);
+        }
+        
+    }
+}
